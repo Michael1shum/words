2c74a277dc6aa792fb4b96407785bed40b7243fc
Miha <mishenka501@yandex.ru>
2024-02-24 15:45:12 +0300

    Убрал ненужные из апи

--- api/src/index.js
+++ api/src/index.js
@@ -1,47 +1,49 @@
 const express = require("express");
-const axios = require("axios");
-const { connectDb } = require("./helpers/db");
-const { port, db, authApiUrl } = require("./configuration");
-const mongoose = require("mongoose");
+// const axios = require("axios");
+// const { connectDb } = require("./helpers/db");
+// const { port, db, authApiUrl } = require("./configuration");
+// const mongoose = require("mongoose");
 const app = express();
 
-const postSchema = new mongoose.Schema({
-  name: String,
-});
+// const postSchema = new mongoose.Schema({
+//   name: String,
+// });
 
-const Post = mongoose.model("Post", postSchema);
+// const Post = mongoose.model("Post", postSchema);
 
 const startServer = () => {
   app.listen(port, async () => {
-    console.log(`Service api started on port: ${port}`);
-    console.log(`DataBase ${db}`);
+    console.log(`Service api started on port: ${3000}`);
+//     console.log(`DataBase ${db}`);
 
-    const silence = new Post({ name: "Silence" });
-    // await silence.save();
+//     const silence = new Post({ name: "Silence" });
+//     // await silence.save();
 
-    const posts = await Post.find();
+//     const posts = await Post.find();
 
-    console.log("posts", posts);
-  });
-};
+//     console.log("posts", posts);
+//   });
+// };
 
 app.get("/test", (req, res) => {
   res.send("Server is working!");
 });
 
-app.get("/testwithCurrentUser", (req, res) => {
-  axios.get(`${authApiUrl}/currentUser`).then((response) => {
-    res.json({ testWithCurrentUser: true, currentUserFromAuth: response.data });
-  });
-});
+startServer();
 
-app.get("/testApiData", (req, res) => {
-  res.json({
-    testWithApi: true,
-  });
-});
+// app.get("/testwithCurrentUser", (req, res) => {
+//   axios.get(`${authApiUrl}/currentUser`).then((response) => {
+//     res.json({ testWithCurrentUser: true, currentUserFromAuth: response.data });
+//   });
+// });
 
-connectDb()
-  .on("error", console.log)
-  .on("disconnect", connectDb)
-  .once("open", startServer);
+// app.get("/testApiData", (req, res) => {
+//   res.json({
+//     testWithApi: true,
+//   });
+// });
+
+// connectDb()
+//   .on("error", console.log)
+//   .on("disconnect", connectDb)
+//   .once("open", startServer);
