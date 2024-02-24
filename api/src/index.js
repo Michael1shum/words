bb3f99943c25d266b3edca1cd51e366419f4a396
grsjob <92042203+grsjob@users.noreply.github.com>
2024-02-24 17:11:30 +0300

    закомментил базу под локальный запуск

--- api/src/index.js
+++ api/src/index.js
@@ -1,47 +1,50 @@
 const express = require("express");
-const axios = require("axios");
-const { connectDb } = require("./helpers/db");
-const { port, db, authApiUrl } = require("./configuration");
-const mongoose = require("mongoose");
+// const axios = require("axios");
+// const { connectDb } = require("./helpers/db");
+// const { port, db, authApiUrl } = require("./configuration");
+// const mongoose = require("mongoose");
 const app = express();
 
-const postSchema = new mongoose.Schema({
-  name: String,
-});
-
-const Post = mongoose.model("Post", postSchema);
+// const postSchema = new mongoose.Schema({
+//   name: String,
+// });
+//
+// const Post = mongoose.model("Post", postSchema);
 
 const startServer = () => {
-  app.listen(port, async () => {
-    console.log(`Service api started on port: ${port}`);
-    console.log(`DataBase ${db}`);
-
-    const silence = new Post({ name: "Silence" });
-    // await silence.save();
-
-    const posts = await Post.find();
-
-    console.log("posts", posts);
+  app.listen(3000, async () => {
+    console.log(`Service api started on port: ${3000}`);
+    // console.log(`DataBase ${db}`);
+    //
+    // const silence = new Post({ name: "Silence" });
+    // // await silence.save();
+    //
+    // const posts = await Post.find();
+    //
+    // console.log("posts", posts);
   });
 };
 
-app.get("/test", (req, res) => {
+app.get("/test2", (req, res) => {
   res.send("Server is working!");
 });
 
-app.get("/testwithCurrentUser", (req, res) => {
-  axios.get(`${authApiUrl}/currentUser`).then((response) => {
-    res.json({ testWithCurrentUser: true, currentUserFromAuth: response.data });
-  });
-});
+// app.get("/testwithCurrentUser", (req, res) => {
+//   axios.get(`${authApiUrl}/currentUser`).then((response) => {
+//     res.json({ testWithCurrentUser: true, currentUserFromAuth: response.data });
+//   });
+// });
 
 app.get("/testApiData", (req, res) => {
   res.json({
     testWithApi: true,
+    status: "good",
   });
 });
 
-connectDb()
-  .on("error", console.log)
-  .on("disconnect", connectDb)
-  .once("open", startServer);
+// connectDb()
+//   .on("error", console.log)
+//   .on("disconnect", connectDb)
+//   .once("open", startServer);
+
+startServer();
