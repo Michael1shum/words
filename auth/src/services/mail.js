d933a7ad9ff9966116dc029d84e6c1171a0cc7fb
grsjob <92042203+grsjob@users.noreply.github.com>
2024-03-24 23:43:28 +0300

    необходимы правки по авторизации в майил сервисе

--- auth/src/services/mail.js
+++ auth/src/services/mail.js
@@ -1,6 +1,34 @@
+const nodeMailer = require('nodemailer')
+const {mailId, mailPassword} = require("../configuration/index");
+
 class MailService {
-  async sendActivationMail(email, link) {
-  }
+
+    constructor() {
+        this.transporter = nodeMailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: "sup.words.el@gmail.com",
+                // user: mailId,
+                pass: "12qwaszx!@QWASZX"
+                // pass: mailPassword
+            }
+        })
+    }
+
+    async sendActivationMail(email, link) {
+        await this.transporter.sendMail({
+            from: mailId,
+            to: email,
+            subject: "Активация аккаунта",
+            text: '',
+            html: `
+            <div>
+                <h1>Для активации перейдите по ссылке</h1>
+                <a href="${link}">${link}</a>
+            </div>
+            `
+        })
+    }
 }
 
 module.exports = new MailService();
--- auth/src/services/user.js
+++ auth/src/services/user.js
@@ -4,26 +4,28 @@ const uuid = require('uuid');
 const MailService = require('./mail');
 const TokenService = require('./token');
 const UserDTO = require('../dtos/user-dto');
+const {appUrl} = require("../configuration/index");
+
 
 class UserService {
-  async registration(email, password) {
-    const candidate = await UserModel.findOne({ email });
-    if (candidate) {
-      throw new Error(`Пользователь с адресом ${email} уже существует!`);
+    async registration(email, password) {
+        const candidate = await UserModel.findOne({email});
+        if (candidate) {
+            throw new Error(`Пользователь с адресом ${email} уже существует!`);
+        }
+        const hashPassword = await bcrypt.hash(password, 3);
+        const activationLink = uuid.v4();
+
+        const user = await UserModel.create({email, password: hashPassword, activationLink});
+        await MailService.sendActivationMail(email, `${appUrl}/api/activate/${activationLink}`);
+
+        const userDto = new UserDTO(user);
+        const tokens = TokenService.generateToken({...userDto});
+
+        await TokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {...tokens, user: userDto};
     }
-    const hashPassword = await bcrypt.hash(password, 3);
-    const activationLink = uuid.v4();
-
-    const user = await UserModel.create({ email, password: hashPassword, activationLink });
-    await MailService.sendActivationMail(email, activationLink);
-
-    const userDto = new UserDTO(user);
-    const tokens = TokenService.generateToken({ ...userDto });
-
-    await TokenService.saveToken(userDto.id, tokens.refreshToken);
-
-    return { ...tokens, user: userDto };
-  }
 }
 
 module.exports = new UserService();
