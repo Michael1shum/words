767e69fdb8ad5e3b8c7bced2672f584384c4dfa0
Роман Гальцев <rvgaltsev@sberbank.ru>
2024-03-25 23:15:26 +0300

    изменения по фронту

--- auth/src/index.js
+++ auth/src/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-app.use('/api', router);
+app.use('/auth', router);
 app.use(errorsMiddleware);
 
 const startServer = () => {
