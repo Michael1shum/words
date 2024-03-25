674d6ceb2756173d87a4aabd310c5799d055e000
Роман Гальцев <rvgaltsev@sberbank.ru>
2024-03-25 10:26:38 +0300

    переключил метод отправки сообщений на mail.ru

--- auth/src/services/mail.js
+++ auth/src/services/mail.js
@@ -1,34 +1,34 @@
-const nodeMailer = require('nodemailer')
-const {mailId, mailPassword} = require("../configuration/index");
+const nodeMailer = require('nodemailer');
+const { mailId, mailPassword } = require('../configuration/index');
 
 class MailService {
 
-    constructor() {
-        this.transporter = nodeMailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: "sup.words.el@gmail.com",
-                // user: mailId,
-                pass: "12qwaszx!@QWASZX"
-                // pass: mailPassword
-            }
-        })
-    }
+  constructor() {
+    this.transporter = nodeMailer.createTransport({
+      host: 'smtp.mail.ru',
+      port: 587,
+      secure: false,
+      auth: {
+        user: mailId,
+        pass: mailPassword,
+      },
+    });
+  }
 
-    async sendActivationMail(email, link) {
-        await this.transporter.sendMail({
-            from: mailId,
-            to: email,
-            subject: "Активация аккаунта",
-            text: '',
-            html: `
+  async sendActivationMail(email, link) {
+    await this.transporter.sendMail({
+      from: mailId,
+      to: email,
+      subject: 'Активация аккаунта',
+      text: '',
+      html: `
             <div>
                 <h1>Для активации перейдите по ссылке</h1>
-                <a href="${link}">${link}</a>
+                <a href='${link}'>${link}</a>
             </div>
-            `
-        })
-    }
+            `,
+    });
+  }
 }
 
 module.exports = new MailService();
--- auth/src/services/user.js
+++ auth/src/services/user.js
@@ -3,29 +3,29 @@ const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const MailService = require('./mail');
 const TokenService = require('./token');
-const UserDTO = require('../dtos/user-dto');
-const {appUrl} = require("../configuration/index");
+const UserDTO = require('../dto/user-dto');
+const { appUrl } = require('../configuration/index');
 
 
 class UserService {
-    async registration(email, password) {
-        const candidate = await UserModel.findOne({email});
-        if (candidate) {
-            throw new Error(`Пользователь с адресом ${email} уже существует!`);
-        }
-        const hashPassword = await bcrypt.hash(password, 3);
-        const activationLink = uuid.v4();
-
-        const user = await UserModel.create({email, password: hashPassword, activationLink});
-        await MailService.sendActivationMail(email, `${appUrl}/api/activate/${activationLink}`);
-
-        const userDto = new UserDTO(user);
-        const tokens = TokenService.generateToken({...userDto});
-
-        await TokenService.saveToken(userDto.id, tokens.refreshToken);
-
-        return {...tokens, user: userDto};
+  async registration(email, password) {
+    const candidate = await UserModel.findOne({ email });
+    if (candidate) {
+      throw new Error(`Пользователь с адресом ${email} уже существует!`);
     }
+    const hashPassword = await bcrypt.hash(password, 3);
+    const activationLink = uuid.v4();
+
+    const user = await UserModel.create({ email, password: hashPassword, activationLink });
+    await MailService.sendActivationMail(email, `${appUrl}/api/activate/${activationLink}`);
+
+    const userDto = new UserDTO(user);
+    const tokens = TokenService.generateToken({ ...userDto });
+
+    await TokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
+  }
 }
 
 module.exports = new UserService();
