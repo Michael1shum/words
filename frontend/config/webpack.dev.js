4d44656b6cc2f78db7ef788c5b94e5a9e2827e8d
grsjob <92042203+grsjob@users.noreply.github.com>
2024-05-11 13:48:22 +0300

    корректировка настроек webpack для работы с docker

--- frontend/config/webpack.dev.js
+++ frontend/config/webpack.dev.js
@@ -1,24 +1,28 @@
-const { merge } = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const common = require('./webpack.common');
 const path = require('path');
 
 module.exports = (env) => {
-  return merge(common(env), {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-      client: {
-        webSocketURL: 'ws://127.0.0.1:3000/ws',
-      },
-      static: {
-        directory: path.join(__dirname, '..', 'public'),
-      },
-      compress: true,
-      port: 3000,
-      allowedHosts: 'all',
-    },
-    watch: {
-      ignored: 'node_modules',
-    },
-  });
+    return merge(common(env), {
+        mode: 'development',
+        devtool: 'inline-source-map',
+        devServer: {
+            client: {
+                webSocketURL: 'ws://127.0.0.1:3000/ws',
+            },
+            static: {
+                directory: path.join(__dirname, '..', 'public'),
+            },
+            compress: true,
+            port: 3000,
+            allowedHosts: 'all',
+        },
+        watch: {
+            ignored: 'node_modules',
+        },
+        watchOptions: {
+            aggregateTimeout: 500, // delay before reloading
+            poll: 1000, // enable polling since fsevents are not supported in docker
+        },
+    });
 };
