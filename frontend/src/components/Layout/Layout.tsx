16701311d1a5a26387a36deb1d6992b6264f1025
grsjob <92042203+grsjob@users.noreply.github.com>
2024-06-21 00:16:54 +0300

    корректировка настроек webpack для работы с docker

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import {Outlet} from 'react-router-dom';
+import {useCookies} from "react-cookie";
 
 export const Layout = () => {
+  const [cookies] = useCookies(['role']);
+  console.log("cookie", cookies.role)
   return (
     <div>
-      <Outlet />
+      <Outlet/>
     </div>
   );
 };
