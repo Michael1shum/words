340e1b39c99d6ec0e97088a0dca8b79ce54e71d2
Galtsev <Galtsev.R.V@sbertech.ru>
2024-12-28 15:20:34 +0300

    добавил вывод айдишников в ДТОшку тестов при запросе

--- api/src/dto/test-dto.js
+++ api/src/dto/test-dto.js
@@ -1,8 +1,10 @@
 module.exports = class TestDTO {
   name;
   questions;
+  id;
 
   constructor(model) {
+    this.id = model._id;
     this.name = model.name;
     this.questions = model.questions.map((questionNumber) => ({
       controlType: questionNumber.controlType,
