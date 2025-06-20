6377ce6a78af3ac6491da3b780eb92ce943a6361
mihael <mishenka501@yandex.ru>
2025-06-20 21:02:30 +0300

    конпка по теории

--- frontend/src/routes/Theory/TheoryListPage.tsx
+++ frontend/src/routes/Theory/TheoryListPage.tsx
@@ -59,6 +59,14 @@ export const TheoryListPage = () => {
           )}
           locale={{ emptyText: 'No materials available' }}
         />
+
+        <Button
+          type="primary"
+          onClick={() => navigate('/add-theory')}
+          style={{ width: '200px' }}
+        >
+          Добавить
+        </Button>
       </Space>
     </div>
   );
